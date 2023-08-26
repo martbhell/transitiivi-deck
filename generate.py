dd9c973b7ba95acb952159c44dcfdb9ed918bfd8
Johan Guldmyr <martbhell@protonmail.com>
2023-08-26 12:03:46 +0300

    parse wiktionary and get explanations

--- generate.py
+++ generate.py
@@ -2,17 +2,46 @@
 
 import genanki
 
+import mediawikiapi
 # scrape wiktionary with beautifulsoup4:
 #  - visit https://en.wiktionary.org/wiki/Category:Finnish_transitive_verbs
 #  - follow links til we get to the end
 #  - https://en.wiktionary.org/wiki/Category:Finnish_intransitive_verbs
 #  - add words and explanation and if transitive or intransitive
 
-# Data structure
+
+def get_words():
+    """ parse wiktionary """
+
+    mediawiki = mediawikiapi.MediaWikiAPI(config=mediawikiapi.Config(mediawiki_url="https://{}.wiktionary.org/w/api.php"))
+
+    trans = mediawiki.category_members(title="Finnish_transitive_verbs", cmlimit=5)
+    intrans = mediawiki.category_members(title="Finnish_intransitive_verbs", cmlimit=5)
+
+    trans_dict = {}
+    intrans_dict = {}
+    for t in trans:
+        t_page = mediawiki.page(t)
+        explanation = t_page.section("Verb")
+        if explanation == "": continue
+        trans_dict[t] = explanation
+
+    for it in intrans:
+        it_page = mediawiki.page(it)
+        in_explanation = it_page.section("Verb")
+        if in_explanation == "": continue
+        intrans_dict[it] = in_explanation
+
+
+    return(trans_dict, intrans_dict)
 
 
 def main():
     """The Thing"""
+
+    trans, intrans = get_words()
+    print(trans)
+    print(intrans)
 
     words = [
         ("verb1", "trans|intrans + explanation"),
