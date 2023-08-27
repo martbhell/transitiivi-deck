1027e9419af33ceaae892d171e52b0e64043d39a
Johan Guldmyr <martbhell@protonmail.com>
2023-08-27 15:04:40 +0300

    insert intransitive explanation
    sure plenty of room to deduplicate and remove issues like this

--- generate.py
+++ generate.py
@@ -133,7 +133,7 @@ def get_words():
         if in_explanation.strip() == "": continue
         intrans_dict[it] = in_explanation
 
-        itinsert = (it, explanation, 0)
+        itinsert = (it, in_explanation, 0)
         create_verb(conn, itinsert)
 
     conn.close()
